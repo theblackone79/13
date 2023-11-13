558dc74395099a5396d123861a1e6007cf896d16
theblackone79 <150225131+theblackone79@users.noreply.github.com>
2023-11-13 14:04:45 +0300

    Add files via upload

---
+++ рудик грачева 1 13.cpp
@@ -0,0 +1,44 @@
+﻿using namespace std;
+#include <iostream>
+
+int main()
+{
+	setlocale(0, "");
+	int count = 0;
+	int number;
+	int sum = 0;
+	int chet = 0;
+
+	while (true)
+	{
+		cout << "Введите число (0 для завершения): ";
+		cin >> number;
+
+		if (number == 0)
+		{
+			break;
+		}
+
+		sum += number;
+		count++;
+		chet++;
+	}
+
+	cout << "Введенные числа: " << endl;
+
+
+
+	if (count > 0) {
+
+		cout << "введенно чисел:" << count << endl;
+
+	}
+
+	cout << "Сумма: " << sum << endl;
+	cout << "Среднее арифметическое: " << sum / chet;
+	cout << endl;
+
+	return 0;
+
+
+}
