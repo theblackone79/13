d64e75f18bf0e634201bbf26d88139d747a00d6a
theblackone79 <150225131+theblackone79@users.noreply.github.com>
2023-11-13 14:19:02 +0300

    Add files via upload

---
+++ рудик грачева 3 13.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int number = 15;
+	long long factorial = 1;
+
+	for (int i = 1; i <= number; ++i)
+	{
+		factorial *= i;
+	}
+
+	cout << "Факториал числа " << number << " равен: " << factorial << endl;
+
+	return 0;
+}
