37d18bc3424b1e61d32aa7aab0fad64b58b535ae
theblackone79 <150225131+theblackone79@users.noreply.github.com>
2023-11-13 14:23:06 +0300

    Add files via upload

---
+++ рудик грачева 4 13.cpp
@@ -0,0 +1,19 @@
+﻿using namespace std;
+#include <iostream>
+int main()
+{
+	setlocale(0, "");
+	int n;
+	cout << "введите число до кокого числа выдавать таблицу: ";
+	cin >> n;
+
+	cout << "число\t\tквадрат\t\tкуб" << endl;
+
+	for (int i = 1; i <= n; ++i) {
+		int square = i * i;
+		int cube = i * i * i;
+		cout << i << "\t\t" << square << "\t\t" << cube << endl;
+	}
+
+	return 0;
+}
