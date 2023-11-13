6571442352528dd897508e6a9f4da24102f54ad1
theblackone79 <150225131+theblackone79@users.noreply.github.com>
2023-11-13 14:09:31 +0300

    Add files via upload

---
+++ рудик грачева 2 13.cpp
@@ -0,0 +1,31 @@
+﻿using namespace std;
+#include <iostream>
+
+int main()
+{
+	setlocale(0, "");
+	int h;
+	cout << "введите высоту фигур: ";
+	cin >> h;
+
+	cout << "равнобедренный треугольник " << endl;
+	for (int i = 1; i <= h; i++)
+	{
+		for (int g = 1; g <= h - i; g++)
+			cout << " ";
+		for (int a = 1; a <= 2 * i - 1; a++)
+			cout << "*";
+		cout << endl;
+	}
+
+	cout << endl << "трапеция" << endl;
+	for (int q = 1; q <= h; q++)
+	{
+		for (int b = 1; b <= h - q; b++)
+			cout << "   ";
+		for (int y = 2; y <= 1 * q - 1; y++)
+			cout << "******";
+		cout << endl;
+	}
+	return 0;
+}
